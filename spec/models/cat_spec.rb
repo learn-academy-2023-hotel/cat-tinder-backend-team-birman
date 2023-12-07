b8a37a9d2b8d410ad47fa59afb78ac0ca82827f6
Justin Pinkard <justin.d.pinkard@gmail.com>
2023-12-07 15:05:05 -0800

    blah

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate name" do 
+    cat = Cat.create(
+      age: 2,
+      enjoys: 'Walks in the park',
+      image: 'sample'
+      )
+    expect(cat.errors[:name]).to_not be_empty
+  end
+
+  it "should validate age" do
+    cat = Cat.create(
+      name: "Ralph",
+      enjoys: "food",
+      image: 'sample2'
+    )
+    expect(cat.errors[:age]).to_not be_empty
+  end
+
+  it "should validate enjoys" do
+    cat = Cat.create(
+      name: "Alex",
+      age: 8,
+      image: 'sample3'
+    )
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+
+  it "should validate image" do
+    cat = Cat.create(
+      name: "Justin",
+      age: 6,
+      enjoys: "no pictures"
+    )
+    expect(cat.errors[:image]).to_not be_empty
+  end
+
+  it "it enjoys should be at least 10 characters long" do
+    cat = Cat.create(
+      name: "Kevin",
+      age: 10,
+      enjoys: "lessthan1",
+      image: "sample4"
+    )
+    expect(cat.errors[:enjoys]).to_not be_empty
+  end
+  
+
 end
--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -73,7 +73,7 @@ RSpec.describe "Cats", type: :request do
         cat: {
           name: 'Felix',
           age: 3,
-          enjoys: 'Food',
+          enjoys: 'Walks in the park',
           image: 'sample'
         }
       }
@@ -83,9 +83,133 @@ RSpec.describe "Cats", type: :request do
       delete "/cats/#{cat.id}"
 
       expect(response).to have_http_status(200)
-      expect(Cat.count).to eq 0
+      expect(Cat.find_by(id: cat.id)).to be_nil
 
     end
   end
+
+  describe "cannot create a cat without valid attributes" do
+    it "doesn't create a cat without a name" do
+      cat_params = {
+        cat: {
+          age: 2,
+          enjoys: 'Walks in the park',
+          image: 'sample'
+        }
+      }
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+
+      cat = JSON.parse(response.body)
+      expect(cat['name']).to include "can't be blank"
+    end
+  end
+
+  describe "cannot create a cat without valid attributes" do
+    it "doesn't create a cat without an age" do
+      cat_params = {
+        cat: {
+          name: "Fred",
+          enjoys: 'Walks in the park',
+          image: 'sample'
+        }
+      }
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      
+      cat = JSON.parse(response.body)
+      expect(cat['age']).to include "can't be blank"
+    end
+  end
+
+  describe "cannot create a cat without valid attributes" do
+    it "doesn't create a cat without an enjoys" do
+      cat_params = {
+        cat: {
+          name: "Fred",
+          age: 8,
+          image: "sample"
+        }
+      }
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+
+      cat = JSON.parse(response.body)
+      expect(cat['enjoys']).to include "can't be blank"
+    end
+  end
+
+  describe "cannot create a cat without valid attributes" do
+    it "doesn't create a cat without an image" do
+      cat_params = {
+        cat: {
+          name: "Fred",
+          age: 8,
+          enjoys: "Hello World"
+        }
+      }
+      post '/cats', params: cat_params
+      expect(response.status).to eq 422
+      
+      cat = JSON.parse(response.body)
+      expect(cat['image']).to include "can't be blank"
+    end
+  end
+
+  # describe "cannot create a cat without valid length in enjoys" do
+  #   it "doesn't create a cat without enjoys having a length less than 10" do
+  #     cat_params = {
+  #       cat: {
+  #         name: "Fred",
+  #         age: 10,
+  #         enjoys: "Hello War",
+  #         image: "sample8"
+  #       }
+  #     }
+  #     post '/cats', params: cat_params
+  #     expect(response.status).to eq 422
+
+  #     cat = Cat.first
+
+  #     # cat = JSON.parse(response.body)
+  #     # expect(cat['enjoys']).to include "can't be less than 10 characters"
+  #     # expect(cat['errors']['enjoys']).to include "is too short(minimum is 10 characters)"
+  #     # expect(cat.enjoys.length).to be >= 10
+  #     # expect(cat['enjoys'].length).to be >= 10
+  #   end
+  # end
+
+  describe "cannot update a cat without valid attributes" do
+    it "doesn't update a cat without an enjoys" do
+      cat_params = {
+        cat: {
+          name: 'Buster',
+          age: 4,
+          enjoys: 'Meow Mix and plenty of sunshine.',
+          image: 'sample'
+        }
+      }
+      post '/cats/', params: cat_params
+      cat = Cat.first
+
+      updated_params = {
+        cat: {
+          name: 'Felix',
+          age: 4,
+          enjoys: 'Meow Mix and plenty of sunshine',
+          image: 'sample'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_params
+
+      updated_cat = Cat.find(cat.id)
+
+      expect(response.status).to eq 422
+
+      cat = JSON.parse(response.body)
+      expect(cat['enjoys']).to include "can't be blank"
+    end
+  end
+
 
 end
